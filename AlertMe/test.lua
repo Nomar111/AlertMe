2aae598825284bd4fcf227c7cdf87e998628e426
Nomar111 <58907487+Nomar111@users.noreply.github.com>
2020-06-03 18:25:20 +0200

    Update test.lua

--- AlertMe/test.lua
+++ AlertMe/test.lua
@@ -1,1 +1,29 @@
--- test
+-- get addon environment
+local AlertMe = _G.AlertMe
+-- wow upvalues
+local print = print
+-- misc upvalues
+local LibStub = LibStub
+-- set addon environment as new global environment
+setfenv(1, AlertMe)
+
+-- init AceGUI
+local AceGUI = LibStub("AceGUI-3.0")
+-- Create a container frame
+local frame = AceGUI:Create("Frame")
+frame:SetTitle("Example Frame")
+frame:SetStatusText("AceGUI-3.0 Example Container Frame")
+frame:SetCallback("OnClose", function(widget) AceGUI:Release(widget) end)
+frame:SetLayout("Flow")
+-- editbox
+local editbox = AceGUI:Create("EditBox")
+editbox:SetLabel("Insert text:")
+editbox:SetWidth(200)
+editbox:SetCallback("OnEnterPressed", function(widget, event, text) textStore = text end)
+frame:AddChild(editbox)
+-- button
+local button = AceGUI:Create("Button")
+button:SetText("Click Me!")
+button:SetWidth(200)
+button:SetCallback("OnClick", function() print(textStore) end)
+frame:AddChild(button)
